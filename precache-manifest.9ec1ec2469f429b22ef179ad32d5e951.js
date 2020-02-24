24b1b876d7dcc42b1c03853bc5d0ca4a50ae2db9
poinke <astairs135@gmail.com>
2020-02-24 22:17:21 +0700

    Updates

---
+++ precache-manifest.9ec1ec2469f429b22ef179ad32d5e951.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aed53f986a6979ff3e072f0cd953600",
+    "url": "/my1app/index.html"
+  },
+  {
+    "revision": "77161c91a3ac0a8c1df5",
+    "url": "/my1app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "5ea47c1098162aa85ab8",
+    "url": "/my1app/static/css/main.37a55137.chunk.css"
+  },
+  {
+    "revision": "77161c91a3ac0a8c1df5",
+    "url": "/my1app/static/js/2.db954ea9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my1app/static/js/2.db954ea9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ea47c1098162aa85ab8",
+    "url": "/my1app/static/js/main.d33d42ff.chunk.js"
+  },
+  {
+    "revision": "6393ed39467173b3aff8",
+    "url": "/my1app/static/js/runtime-main.53c41f1b.js"
+  }
+]);
